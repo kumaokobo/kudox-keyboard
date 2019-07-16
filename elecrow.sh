efa0fc2293223789ab925696b5f04dc65f25884a
x1 <viva008@gmail.com>
2019-07-16 15:33:23 +0900

    [kudox] Fix lack of wiring at D21 on Kudox PCB. (#8)

---
+++ elecrow.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIR=$1
+
+if [ -z "$DIR" ]; then
+	echo "Usage: elecrow.sh /path/to/gerber"
+	exit 1
+fi
+
+declare -a KEYS=("-F.Cu.gbr" "-B.Cu.gbr" "-F.Mask.gbr" "-B.Mask.gbr" "-F.SilkS.gbr" "-B.SilkS.gbr" "-F.Paste.gbr" "-B.Paste.gbr" "-PTH.drl" "-NPTH.drl" "-Edge.Cuts.gbr")
+declare -a EXTS=(".GTL" ".GBL" ".GTS" ".GBS" ".GTO" ".GBO" ".GTP" ".GBP" ".TXT" "-NPTH.TXT" ".GML")
+
+for ((i = 0; i < ${#KEYS[@]}; i++)) {
+	KEY=${KEYS[i]}
+	EXT=${EXTS[i]}
+
+	for file in `find $DIR -name "*$KEY"`
+	do
+		new_file=${file/$KEY/$EXT}
+		echo "mv $file $new_file"
+		mv $file $new_file
+	done
+}
+
